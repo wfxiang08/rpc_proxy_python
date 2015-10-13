530975550f26c946e55c4ffd33106c86ac420d71
feiwang <wangfei@chunyu.me>
2015-10-13 14:51:51 +0800

    添加了arg_format参数，可以控制log的格式

--- rpc_thrift/rpc_wrapper.py
+++ rpc_thrift/rpc_wrapper.py
@@ -4,7 +4,11 @@ import time
 import traceback
 from rpc_thrift.services.ttypes import RpcException
 
-def rpc_wrapper_for_class_method(info_logger):
+# log_func
+# def log_func(*args, **kwargs, result = None, ex = None, trace=None):
+#
+#
+def rpc_wrapper_for_class_method(info_logger, arg_format=None):
     """
     计算类成员函数的执行时间&异常处理, 使用方法(其中logger可以为None)
         @calculate_class_fun_execute_time(logger)
@@ -16,15 +20,24 @@ def rpc_wrapper_for_class_method(info_logger):
             try:
                 if info_logger:
                     t = time.time()
+
+                    # rpc调用中没有kwargs
                     result = func(*args, **kwargs)
+
                     t = (time.time() - t) * 1000
-                    args_str = ", ".join(map(str, args[1:]))
+                    if arg_format:
+                        args_str = arg_format(*args[1:])
+                    else:
+                        args_str = ", ".join(map(str, args[1:]))
                     info_logger.info('\033[32m%s\033[39m(%s), Elapsed: %.2fms' % (func.__name__, args_str, t))
                 else:
                     result = func(*args, **kwargs)
                 return result
             except Exception as e:
-                args_str = ", ".join(map(str, args[1:]))
+                if arg_format:
+                    args_str = arg_format(*args[1:])
+                else:
+                    args_str = ", ".join(map(str, args[1:]))
                 raise RpcException(0, '%s(%s), Exception: %s, Trace: %s' % (func.__name__, args_str, str(e), traceback.format_exc()))
 
         return _calculate_time
@@ -32,7 +45,7 @@ def rpc_wrapper_for_class_method(info_logger):
 
 
 
-def rpc_wrapper_for_method(info_logger):
+def rpc_wrapper_for_method(info_logger, arg_format=None):
     """
     计算普通函数的执行时间&异常处理, 使用方法(其中logger可以为None)
         @calculate_class_fun_execute_time(logger)
@@ -46,13 +59,19 @@ def rpc_wrapper_for_method(info_logger):
                     t = time.time()
                     result = func(*args, **kwargs)
                     t = (time.time() - t) * 1000
-                    args_str = ", ".join(map(str, args))
+                    if arg_format:
+                        args_str = arg_format(*args)
+                    else:
+                        args_str = ", ".join(map(str, args))
                     info_logger.info('\033[32m%s\033[39m(%s), Elapsed: %.4fms' % (func.__name__, args_str, t))
                 else:
                     result = func(*args, **kwargs)
                 return result
             except Exception as e:
-                args_str = ", ".join(map(str, args))
+                if arg_format:
+                    args_str = arg_format(*args)
+                else:
+                    args_str = ", ".join(map(str, args))
                 raise RpcException(0, '%s(%s), Exception: %s, Trace: %s' % (func.__name__, args_str, str(e), traceback.format_exc()))
 
         return _calculate_time
